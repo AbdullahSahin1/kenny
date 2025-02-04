3f92795df84fd617a97cf5c3c045e3687ab1f0e3
AbdullahSahin1 <sahinabdullah2003@hotmail.com>
2025-02-04 12:48:31 +0300

    projenin bitişi

--- app/src/main/java/com/example/examplekenny/MainActivity.kt
+++ app/src/main/java/com/example/examplekenny/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.examplekenny
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.os.Handler
 import android.os.Looper
 import android.view.View
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -35,14 +38,46 @@ class MainActivity : AppCompatActivity() {
         imageArray.add(binding.imageView8)
         imageArray.add(binding.imageView9)
         hideImages()
-        object : CountDownTimer(150000,1000){
-            override fun onTick(millisUntilFinished: Long) {
-                binding.timeText.text = "Time: ${millisUntilFinished/1000}"
-            }
-
+        object : CountDownTimer(15000,1000){
             override fun onFinish() {
 
+                binding.timeText.text = "Time: 0"
+
+                handler.removeCallbacks(runnable)
+
+                for (image in imageArray) {
+                    image.visibility = View.INVISIBLE
+                }
+
+
+
+                //Alert
+                val alert = AlertDialog.Builder(this@MainActivity)
+
+                alert.setTitle("Game Over")
+                alert.setMessage("Restart The Game?")
+                alert.setPositiveButton("Yes") {dialog, which ->
+                    //Restart
+                    val intent = intent
+                    finish()
+                    startActivity(intent)
+
+
+                }
+
+                alert.setNegativeButton("No") {dialog, which ->
+                    Toast.makeText(this@MainActivity,"Game Over",Toast.LENGTH_LONG).show()
+                }
+
+                alert.show()
+
+
             }
+
+            override fun onTick(millisUntilFinished: Long) {
+                binding.timeText.text = "Time: " + millisUntilFinished/1000
+            }
+
 
         }.start()
     }
