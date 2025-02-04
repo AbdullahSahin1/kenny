7f302dba1110abacd3b649236bd1cd85207356ba
AbdullahSahin1 <sahinabdullah2003@hotmail.com>
2025-02-04 10:40:48 +0300

    Score Güncellendi

--- app/src/main/java/com/example/examplekenny/MainActivity.kt
+++ app/src/main/java/com/example/examplekenny/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import android.os.Handler
 import android.os.Looper
+import android.view.View
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -12,6 +13,7 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.examplekenny.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    var score = 0
     private lateinit var binding: ActivityMainBinding
     var imageArray = ArrayList<ImageView>()
     var handler = Handler(Looper.getMainLooper())
@@ -21,5 +23,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+    }
+    fun increaseScore(view: View) {
+        score = score + 1
+        binding.scoreText.text = "Score:${score} "
     }
 }
